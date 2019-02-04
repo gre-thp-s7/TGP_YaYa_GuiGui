6c8d861eead1da4dc66e371f38aae97ff2d70519
dendiflet <bretin.iannis@gmail.com>
2019-02-04 15:04:33 +0100

    route static/team & contact transfo en /team & /contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'static/team', to: "static#team"
-  get 'static/contact'
+  get '/team', to: "static#team"
+  get 'contact', to: "static#contact"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
