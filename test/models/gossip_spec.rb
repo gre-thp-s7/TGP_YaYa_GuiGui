770c369d3c320c9497a04d624a1954c34189e971
JungleWilly <guillaumec.dev@gmail.com>
2019-02-05 02:28:03 +0100

    ajout JS ma gueule

--- test/models/gossip_spec.rb
+++ test/models/gossip_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require 'rspec-rails'
 
 RSpec.describe Gossip, type: :model do
 	
--- test/models/tag_spec.rb
+++ test/models/tag_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require 'rspec-rails'
 
 RSpec.describe Tag, type: :model do
 
