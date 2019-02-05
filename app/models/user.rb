aeb9228b734fd3d6a7d0d73d3281233e5d70eff0
dendiflet <bretin.iannis@gmail.com>
2019-02-05 02:07:48 +0100

    mineur rectification apres mon push

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
 	validates :first_name, presence: true
   validates :last_name, presence: true
 	validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "email adress please" }
-end
+
 end
